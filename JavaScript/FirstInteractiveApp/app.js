86290db39ea42f63e47fab470e1e13511d414f54
annu025 <anundhara.c@gmail.com>
2019-01-14 10:07:14 -0800

    Attaching an event listener to the DOM element

--- JavaScript/FirstInteractiveApp/app.js
+++ JavaScript/FirstInteractiveApp/app.js
@@ -1,6 +1,15 @@
-var main = function () { 
+var main = function () {
 	"use strict";
-    window.alert("hello world!");
+	$(".comment-input button").on("click", function (event) {
+		console.log("Hello World!");
+	});
 };
-
 $(document).ready(main);
+
+/*
+What’s happening here? 
+This code is attaching an event listener to the DOM element referenced in the call to 
+the $ function. Notice that the contents of the $ look a lot like a CSS selector—and 
+that’s no coincidence! That’s exactly what it is! jQuery allows you to easily target 
+elements in the DOM using CSS selectors, and then manipulate them using JavaScript.
+*/
