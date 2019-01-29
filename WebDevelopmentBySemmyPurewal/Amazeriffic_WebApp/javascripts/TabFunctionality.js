26f48a0adc9ea5906d5503bd06cf748d390b0c46
annu025 <anundhara.c@gmail.com>
2019-01-29 12:00:45 -0800

    Refactoring tab functionality script

---
+++ WebDevelopmentBySemmyPurewal/Amazeriffic_WebApp/javascripts/TabFunctionality.js
@@ -0,0 +1,22 @@
+var main = function () {
+	"use strict";
+	var makeTabActive = function (tabNumber) {
+
+		// construct the selector from the tabNumber
+		var tabSelector = ".tabs a:nth-child(" + tabNumber + ") span";
+		$(".tabs span").removeClass("active");
+		$(tabSelector).addClass("active");
+	};
+	$(".tabs a:nth-child(1)").on("click", function () {
+		makeTabActive(1);
+		return false;
+	});
+	$(".tabs a:nth-child(2)").on("click", function () {
+		makeTabActive(2);
+		return false;
+	});
+	$(".tabs a:nth-child(3)").on("click", function () {
+		makeTabActive(3);
+		return false;
+	});
+};
